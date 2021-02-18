7dc037cc26001ffdd8108b6dbe884d3c95f87a4b
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-02-17 22:27:11 -0500

    added all prompts and checker on the prompts.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers =["0","1","2","3","4","5","6","7","8","9"];
 var special =[ "!", "#", "$", "%", "&", ")", "(", "*", "+", "?", "@", "~" ];
+var password=[""];
 
 // Write password to the #password input
 function writePassword() {
@@ -33,12 +34,71 @@ var lengthPrompt = function() {
 
 var lowerCasePrompt = function() {
   var answerLowerCase = window.prompt("Lowercase letters? (Y/N)");
-  if (answerLowerCase === "Y") {
+  if (!answerLowerCase) {
+    window.alert("Please choose yes or no");
+    console.log("they did not make a choice");
+    lowerCasePrompt();
+  }
+  else if (answerLowerCase === "Y"){
     console.log("the user picked lowercase password");
     return;
   }
   else {
-  console.log("The user did not pick lowercase password");
+  console.log ("The user did not pick lowercase password");
+  return;
+  }
+};
+
+var upperCasePrompt = function() {
+  var answerUpperCase = window.prompt("Uppercase letters? (Y/N)");
+  if (!answerUpperCase) {
+    window.alert("Please choose yes or no");
+    console.log("they did not make a choice");
+    upperCasePrompt();
+  }
+  else if (answerUpperCase === "Y") {
+    //there needs to be a function that runs there to join the array
+    console.log("the user picked uppercase password");
+    return;
+  }
+  else {
+  console.log("The user did not pick uppercase password");
+  return;
+  }
+};
+
+var numberPrompt = function() {
+  var answerNumber = window.prompt("Numbers? (Y/N)");
+  if (!answerNumber) {
+    window.alert("Please choose yes or no");
+    console.log("they did not make a choice");
+    numberPrompt();
+  }
+  else if (answerNumber === "Y") {
+    //there needs to be a function that runs there to join the array
+    console.log("the user picked numbers in their password");
+    return;
+  }
+  else{
+  console.log("The user did not pick numbers in their password");
+  return;
+  }
+};
+
+var specialPrompt = function() {
+  var answerSpecial = window.prompt("Special Characters? (Y/N)");
+  if (!answerSpecial) {
+    window.alert("Please choose yes or no");
+    console.log("they did not make a choice");
+    specialPrompt();
+  }
+  else if (answerSpecial === "Y") {
+    //there needs to be a function that runs there to join the array
+    console.log("the user picked Special Characters in their password");
+    return;
+  }
+  else{
+  console.log("The user did not pick Special Characters in their password");
   return;
   }
 };
@@ -49,4 +109,11 @@ var generatePassword = function(){
   lengthPrompt();
 //run the lowercase function
   lowerCasePrompt();
+//run the uppercase prompt
+  upperCasePrompt();
+//run the number prompt
+  numberPrompt();
+//run the special characters prompt
+  specialPrompt();
+//
 };

e344e593e7b1649cedc609a6be6fa972bfd57702
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-02-19 17:56:57 -0500

    got password generator running with minor errors.

--- script.js
+++ script.js
@@ -27,134 +27,46 @@ var generatePassword = function(){
   }
   else if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Please choose a number between 8 - 128 characters");
-    generatePassword();
+    console.log("test error one");
+    return;
   }
 //checks to see if the number entered is a number
   else if(isNaN(passwordLength)){
     window.alert("Please choose a number not a character");
-    generatePassword();
+    console.log("test error two")
+    return;
   }
   else if (passwordLength >= 8 || passwordLength <= 128) {
     console.log(passwordLength);
   }
 
-};
 
+//confirm password with options window
+  var lowerCase = confirm("Would you like to include lowercase letters in your password?");
+  console.log(lowerCase);
+  var upperCase = confirm("Would you like to include uppercase letters in your password?");
+  console.log(upperCase);
+  var numbers = confirm("Would you like to include numbers in your password?");
+  console.log(numbers);
+  var special = confirm("Would you like to include special characters in your password?");
+  console.log(special);
+  var combinePassword = "";
 
-var lengthPrompt = function() {
-  var lengthChoice = window.prompt("Enter a number between 8 - 128 characters:");
-  // If user pressed Cancel, immediately end function
-  if (!lengthChoice) {
-    window.alert("Please choose a number between 8 - 128 characters");
-    lengthPrompt();
+  if (lowerCase) {
+  combinePassword = lowerCaseString;
   }
-  else if (lengthChoice < 8 || lengthChoice > 128) {
-    window.alert("Please choose a number between 8 - 128 characters");
-    lengthPrompt();
+  if (upperCase) {
+  combinePassword = combinePassword + upperCaseString;
   }
-  else if(isNaN(lengthChoice)){
-    window.alert("Please choose a number not a character");
-    lengthPrompt();
+  if (numbers) {
+  combinePassword = combinePassword + numbersString;
   }
-  else if (lengthChoice >= 8 || lengthChoice <= 128) {
-    console.log(lengthChoice);
-    return;
-  }
-};
+  if (special) {
+  combinePassword = combinePassword + specialString;
 
-var lowerCasePrompt = function() {
-  var inputLowerCase = window.prompt("Would you like Lowercase letters? (Y/N)");
-  var answerLowerCase = inputLowerCase.toUpperCase();
-  console.log(answerLowerCase);
-
-  if (!answerLowerCase) {
-    window.alert("Please enter Y or N");
-    console.log("they did not make a choice");
-    lowerCasePrompt();
+  for ( var i = 0; i < passwordLength; i++ ) {
+  password += combinePassword[Math.floor(Math.random() * combinePassword.length)];
   }
-  else if (answerLowerCase === "Y"){
-    console.log("the user picked lowercase password");
-    return;
-  }
-  else if (answerLowerCase === "N"){
-    console.log ("The user did not pick lowercase password");
-    return;
-  }
-  else if (answerLowerCase !== "N" || answerLowerCase !== "Y"){
-    window.alert("Please enter Y or N");
-    console.log("they did not enter the right character");
-    lowerCasePrompt();
-  }
-};
-
-var upperCasePrompt = function() {
-  var inputUpperCase = window.prompt("Would you like Uppercase letters? (Y/N)");
-  var answerUpperCase = inputUpperCase.toUpperCase();
-
-  if (!answerUpperCase) {
-    window.alert("Please enter Y or N");
-    console.log("they did not make a choice");
-    upperCasePrompt();
-  }
-  else if (answerUpperCase === "Y") {
-    console.log("the user picked uppercase password");
-    return;
-  }
-  else if (answerUpperCase === "N"){
-    console.log ("The user did not pick uppercase password");
-    return;
-  }
-  else if (answerUpperCase !== "N" || answerUpperCase !== "Y"){
-    window.alert("Please enter Y or N");
-    console.log("they did not enter the right character");
-    upperCasePrompt();
-  }
-};
-
-var numberPrompt = function() {
-  var inputNumber = window.prompt("Would you like Numbers? (Y/N)");
-  var answerNumber = inputNumber.toUpperCase();
-
-  if (!answerNumber) {
-    window.alert("Please enter Y or N");
-    console.log("they did not make a choice");
-    numberPrompt();
-  }
-  else if (answerNumber === "Y") {
-    console.log("the user picked numbers in their password");
-    return;
-  }
-  else if (answerNumber === "N"){
-    console.log ("The user did not pick numbers in their password");
-    return;
-  }
-  else if (answerNumber !== "N" || answerNumber !== "Y"){
-    window.alert("Please enter Y or N");
-    console.log("they did not enter the right character");
-    numberPrompt();
-  }
-};
-
-var specialPrompt = function() {
-  var inputAnswer = window.prompt("Would you like Special Characters? (Y/N)");
-  var answerSpecial = inputAnswer.toUpperCase();
-
-  if (!answerSpecial) {
-    window.alert("Please enter Y or N");
-    console.log("they did not make a choice");
-    specialPrompt();
-  }
-  else if (answerSpecial === "Y") {
-    console.log("the user picked Special Characters in their password");
-    return;
-  }
-  else if (answerSpecial === "N"){
-    console.log ("The user did not pick lowercase password");
-    return;
-  }
-  else if (answerSpecial !== "N" || answerSpecial !== "Y"){
-    window.alert("Please enter Y or N");
-    console.log("they did not enter the right character");
-    specialPrompt();
+  return password;
   }
 };
